4edb1112d01e138d0bac3f54c0bad6858126a927
Jacob <hahkobeh@gmail.com>
2021-12-04 19:49:43 -0700

    building out structure

--- src/main/java/presentation/View.java
+++ src/main/java/presentation/View.java
@@ -1,8 +1,34 @@
 package presentation;
 import application.*;
 
-public class View {
+
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class View extends JFrame {
+    private JLabel title = new JLabel("Property Rental Management System");
+    private JButton button = new JButton();
+    private JTextField testField = new JTextField(10);
+
+
     public View() {
-        System.out.println("hello");
+        JPanel panel = new JPanel();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600,600);
+        panel.add(title);
+        panel.add(button);
+
+
+        this.add(panel); //add panel to frame (view is the JFrame)
     }
+
+    public void addButtonListener(ActionListener listenForButtonPress){
+        button.addActionListener(listenForButtonPress);
+    }
+
+    public void setTestField(String test){
+        testField.setText(test);
+    }
+
+
 }
