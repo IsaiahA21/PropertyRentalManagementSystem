5d66ec49be6cbf31859a079fd7eb202a5da7a9c1
Jacob <hahkobeh@gmail.com>
2021-12-06 00:34:30 -0700

    Database connections established and access starting to work

--- src/main/java/database/DatabaseModel.java
+++ src/main/java/database/DatabaseModel.java
@@ -1,60 +1,150 @@
 package database;
+
+import com.mongodb.MongoException;
+import database.properties.Property;
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import static com.mongodb.client.model.Filters.eq;
 
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import database.properties.Properties;
-import database.users.Landlord;
-import database.users.User;
+
+import database.properties.*;
+import database.users.*;
+import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DatabaseModel {
     private Properties properties;
     private User user;
-    private static int id;
+
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+    private MongoCollection<Document> userCollection;
+    private MongoCollection<Document> propertiesCollection;
 
 
-
-    private static MongoClient mongoClient;
-    private static DB database;
-    private static DBCollection userCollection;
-    private static DBCollection propertiesCollection;
     public DatabaseModel(){
         //
-        id = 0;
 
 
         connectToDatabase();
 
+        user = login("jacob@gmail.com","password12");
+        if(user.getAccessLevel() != 0) {
+            RegisteredUser temp = (RegisteredUser) user;
+            System.out.println("logged in as " + temp.getName());
+        }
 
 
 
-        User test =  new Landlord("jeff@gmail.com","jeff robertson","abc123");
-        System.out.println(test.getAccessLevel());
+        //System.out.println(test.getAccessLevel());
     }
 
     void connectToDatabase(){
-        mongoClient = new MongoClient(new MongoClientURI("mongodb+srv://jacob_artuso:123pass123@prms.vrama.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"));
-        database = mongoClient.getDB("PRMS");
-        userCollection = database.getCollection("userCollection");
-        /*ConnectionString connectionString = new ConnectionString("mongodb+srv://jacob_artuso:<password>@prms.vrama.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-        MongoClientSettings settings = MongoClientSettings.builder()
-                .applyConnectionString(connectionString)
-                .build();
-        MongoClient mongoClient = MongoClients.create(settings);
-        MongoDatabase database = mongoClient.getDatabase("test");*/
+        Logger mongoLogger = Logger.getLogger( "org.mongodb.driver" );
+        mongoLogger.setLevel(Level.SEVERE);
+
+        String uri = "mongodb+srv://jacob_artuso:123pass123@prms.vrama.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+        try {
+            mongoClient = MongoClients.create(uri);
+            database = mongoClient.getDatabase("PRMS");
+            propertiesCollection = database.getCollection("properties");
+            userCollection = database.getCollection("users");
+            addProperty("dave@gmail.com",2,4,false,"condo",20000);
+            addUser(1,"jacob@gmail.com","Jacob Art","password12");
+            addUser(2,"dave@gmail.com","Dave Duart","pass12");
+            addUser(3,"stuart@gmail.com","Stuart Lee","managerpass");
+
+
+        }catch (MongoException me){
+            System.err.println("database error");
+        }
+
+
     }
 
     void addProperty(String landlord, int numBedrooms, int numBathrooms, boolean furnished, String propertyType, double price) {
 
-        testCol.insertOne(convertProperty(new Property(id++, landlord, numBedrooms, numBathrooms, furnished, propertyType, price)));
+        propertiesCollection.insertOne(convertProperty(new Property(new ObjectId().toString(), landlord, numBedrooms, numBathrooms, furnished, propertyType, price)));
     }
 
     public static Document convertProperty(Property prop){
-        return new Document("ID", new ObjectId()).append("STATUS", prop.getStatus()).append("LANDLORD",prop.getLandlord()).append("PAYEDFOR", prop.isPayedFor()).append("OUTSTANDINGFEE", prop.getOutstandingFee()).append("NUMBEDROOMS", prop.getNumBedrooms()).append("NUMBATHROOMS", prop.getNumBathrooms()).append("FURNISHED",prop.isFurnished()).append("PROPERTYTPYE", prop.getPropertyType()).append("PRICE", prop.getPrice());
+        return new Document("_id", new ObjectId()).append("STATUS", prop.getStatus()).append("LANDLORD",prop.getLandlord()).append("PAYEDFOR", prop.isPayedFor()).append("OUTSTANDINGFEE", prop.getOutstandingFee()).append("NUMBEDROOMS", prop.getNumBedrooms()).append("NUMBATHROOMS", prop.getNumBathrooms()).append("FURNISHED",prop.isFurnished()).append("PROPERTYTPYE", prop.getPropertyType()).append("PRICE", prop.getPrice());
 
     }
+
+    User login(String email, String password){
+        //verify with database
+        Document query = new Document("$and", Arrays.asList(
+                new Document("EMAIL", email),
+                new Document("PASSWORD", password)
+        ));
+        Document user = userCollection.find(query).first();
+
+        if (user != null) {
+            switch ((int) user.get("TYPE")) {
+                case 1 -> {
+                    return new RegisteredRenter((String) user.get("EMAIL"), (String) user.get("NAME"), (String) user.get("PASSWORD"));
+                }
+                case 2 -> {
+                    return new Landlord((String) user.get("EMAIL"), (String) user.get("NAME"), (String) user.get("PASSWORD"));
+                }
+                case 3 -> {
+                    return new Manager((String) user.get("EMAIL"), (String) user.get("NAME"), (String) user.get("PASSWORD"));
+                }
+            }
+        }
+        return null;
+    }
+
+    boolean userExists(String email){
+        Document query = new Document("EMAIL", email);
+        return userCollection.find(query).first() != null;
+    }
+
+    void unregistedLogin(){
+        user = new RegularRenter();
+    }
+
+    public ArrayList<Property> search(){
+        return new ArrayList<>();
+    }
+
+    void addUser(int accessLevel, String email, String name, String password){
+        if(userExists(email)){
+            return;
+        }
+        switch (accessLevel){
+            case 0:
+                return;
+            case 1:
+                user = new RegisteredRenter(email,name,password);
+                break;
+            case 2:
+                user = new Landlord(email,name,password);
+                break;
+            case 3:
+                user = new Manager(email,name,password);
+        }
+        userCollection.insertOne(convertUser((RegisteredUser) user));
+    }
+
+    public static Document convertUser(RegisteredUser user){
+        return new Document("_id", new ObjectId()).append("TYPE",user.getAccessLevel()).append("NAME",user.getName()).append("EMAIL", user.getEmail()).append("PASSWORD",user.getPassword());
+    }
+
+
+
+
+
+
 
 
 }
--- src/main/java/database/properties/Property.java
+++ src/main/java/database/properties/Property.java
@@ -1,7 +1,7 @@
 package database.properties;
 
 public class Property {
-    private int propertyID;
+    private String propertyID;
     private int status;//0 not shown, 1 payed for, 2 suspended?
     private String landlord;
     private boolean payedFor;
@@ -14,7 +14,7 @@ public class Property {
 
 
     //create a new property to add
-    public Property(int propertyID, String landlord, int numBedrooms, int numBathrooms, boolean furnished, String propertyType, double price) {
+    public Property(String propertyID, String landlord, int numBedrooms, int numBathrooms, boolean furnished, String propertyType, double price) {
         this.propertyID = propertyID;
         this.landlord = landlord;
         this.numBedrooms = numBedrooms;
@@ -53,4 +53,59 @@ public class Property {
         this.furnished = furnished;
     }
 
+    public String getPropertyID() {
+        return propertyID;
+    }
+
+    public void setPropertyID(String propertyID) {
+        this.propertyID = propertyID;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getLandlord() {
+        return landlord;
+    }
+
+    public void setLandlord(String landlord) {
+        this.landlord = landlord;
+    }
+
+    public boolean isPayedFor() {
+        return payedFor;
+    }
+
+    public void setPayedFor(boolean payedFor) {
+        this.payedFor = payedFor;
+    }
+
+    public double getOutstandingFee() {
+        return outstandingFee;
+    }
+
+    public void setOutstandingFee(double outstandingFee) {
+        this.outstandingFee = outstandingFee;
+    }
+
+    public String getPropertyType() {
+        return propertyType;
+    }
+
+    public void setPropertyType(String propertyType) {
+        this.propertyType = propertyType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
